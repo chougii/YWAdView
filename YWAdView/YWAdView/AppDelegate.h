d3e32069c80f1bd8a92d3f5b48787082926cf0fb
chougii <281435246@qq.com>
2016-02-16 11:26:55 +0800

    first commit

---
+++ YWAdView/YWAdView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YWAdView
+//
+//  Created by ChouGii on 16/2/16.
+//  Copyright © 2016年 zyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YWAdView/YWAdView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YWAdView
+//
+//  Created by ChouGii on 16/2/16.
+//  Copyright © 2016年 zyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
