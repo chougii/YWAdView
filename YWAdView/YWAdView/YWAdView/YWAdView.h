b8c69854a02e76b3f2281a70d9448c658aecea20
chougii <281435246@qq.com>
2016-02-16 11:29:26 +0800

    create

---
+++ YWAdView/YWAdView/YWAdView/YWAdView.h
@@ -0,0 +1,13 @@
+//
+//  YWAdView.h
+//  YWAdView
+//
+//  Created by ChouGii on 16/2/16.
+//  Copyright © 2016年 zyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YWAdView : UIScrollView
+
+@end
---
+++ YWAdView/YWAdView/YWAdView/YWAdView.m
@@ -0,0 +1,21 @@
+//
+//  YWAdView.m
+//  YWAdView
+//
+//  Created by ChouGii on 16/2/16.
+//  Copyright © 2016年 zyw. All rights reserved.
+//
+
+#import "YWAdView.h"
+
+@implementation YWAdView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
